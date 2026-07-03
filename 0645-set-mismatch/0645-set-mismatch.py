fbc5b2b7a9bdc4eefa71fc0ee6ad0cc4474082ad
Abhishek Charadva <charadvaabhi@gmail.com>
2026-07-03 08:39:10 +0530

    Time: 6 ms (88.42%), Space: 21.3 MB (7.46%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findErrorNums(self, nums):
+        n = len(nums)  # [1, 2, 2, 4]  # 4
+        actual_sum = sum(nums)  # 1 + 2 + 2 + 4 = 9
+        unique_sum = sum(set(nums))  # sum({1, 2, 4}) = 7
+        expected_sum = n * (n + 1) // 2  # 4 * (5) // 2 = 10
+
+        duplicate = actual_sum - unique_sum  # 9 - 7 = 2
+        missing = expected_sum - unique_sum  # 10 - 7 = 3
+        return [duplicate, missing]  # [2, 3]
