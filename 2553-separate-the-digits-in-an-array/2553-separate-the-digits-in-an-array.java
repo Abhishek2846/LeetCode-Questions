3a96b8169c7afa4b9d57b650e6780b4852b00926
Abhishek Charadva <charadvaabhi@gmail.com>
2026-08-06 21:56:00 +0530

    Time: 3 ms (94.21%), Space: 46.8 MB (31.42%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
@@ -1,25 +1,41 @@
 class Solution {
     public int[] separateDigits(int[] nums) {
-         List<Integer> result = new ArrayList<>();
+        // List<Integer> result = new ArrayList<>();
+
+        // for (int num : nums) {
+        //     List<Integer> digits = new ArrayList<>();
+        //     while (num > 0) {
+        //         digits.add(num % 10);
+        //         num /= 10;
+        //     }
+
+        //     // Reverse to maintain left-to-right order
+        //     for (int i = digits.size() - 1; i >= 0; i--) {
+        //         result.add(digits.get(i));
+        //     }
+        // }
+
+        // int[] ans = new int[result.size()];
+
+        // for (int i = 0; i < result.size(); i++) {
+        //     ans[i] = result.get(i);
+        // }
+        // return ans;
+
+        //Easy method
+        StringBuilder sb = new StringBuilder();
 
         for (int num : nums) {
-            List<Integer> digits = new ArrayList<>();
-            while (num > 0) {
-                digits.add(num % 10);
-                num /= 10;
-            }
-
-            // Reverse to maintain left-to-right order
-            for (int i = digits.size() - 1; i >= 0; i--) {
-                result.add(digits.get(i));
-            }
+            sb.append(num);
         }
 
-        int[] ans = new int[result.size()];
-        
-        for (int i = 0; i < result.size(); i++) {
-            ans[i] = result.get(i);
+        int length = sb.length();
+        int[] result = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            result[i] = sb.charAt(i) - '0';
         }
-        return ans;
+
+        return result;
     }
 }
